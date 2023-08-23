47790d51d275e76b9414f85ca98d305a904a6d2f
Alan <alan.uthuppan@temple.edu>
2023-08-23 11:59:34 -0400

    Remodularized code, fixed writing and formatting

--- code/main.py
+++ code/main.py
@@ -1,23 +1,84 @@
-import scraper
-import extract
-import process
+from scraper import scrape
+from extract import extract
+from process import table_to_dataframe
 from bs4 import BeautifulSoup
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import Alignment
+from openpyxl.styles import Font
+from openpyxl import Workbook
 import requests
+import os
 
-# headers = {'user-agent': "alanuthuppan@yahoo.com"}
+headers = {'user-agent': "alanuthuppan@yahoo.com"}
 
-# response = requests.get("https://www.sec.gov/Archives/edgar/data/1803498/000180349823000012/bcred-20230331.htm", headers=headers)
-# soup = BeautifulSoup(response.content, 'html.parser')
+response = requests.get("https://www.sec.gov/Archives/edgar/data/1803498/000180349823000012/bcred-20230331.htm", headers=headers)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-# Scrape the pages from the CIK 
-cik = 1803498
-filing_soups = scraper.scrape(cik)
 
-# Extract all the relevant tables and dates from each filing
-tables = []
-for soup in filing_soups:
-    tables.append(extract.extract(soup))
+def write_to_excel(df_tables):
+    filename = "cleaned_soi_tables.xlsx"
 
-# Process each table into Excel format
-for table in tables:
-    process.process(table)
+    if os.path.exists(filename):
+        book = load_workbook(filename)
+    else:
+        book = Workbook()
+        del book["Sheet"]  # Remove default sheet
+
+    for df, date in df_tables:
+        suffix = 1
+        sheet_name = f"{date}_{suffix}"
+
+        # Check if the sheet already exists. If it does, find a new name
+        while sheet_name in book.sheetnames:
+            suffix += 1
+            sheet_name = f"{date}_{suffix}"
+            
+
+        # Create new sheet
+        ws = book.create_sheet(title=sheet_name)
+
+        # Write data
+        for r_idx, row in enumerate(df.values, 1):
+            for c_idx, value in enumerate(row, 1):
+                ws.cell(row=r_idx, column=c_idx, value=value)
+
+        # Write headers
+        for c_idx, header in enumerate(df.columns, 1):
+            cell = ws.cell(row=1, column=c_idx, value=header)
+            cell.font = Font(bold=True)
+
+        # Adjusting the column widths
+        for column in df:
+            max_len = max(df[column].astype(str).apply(len).max(),  # max length in column
+                          len(str(column))) + 2  # length of column header/title
+            col_letter = get_column_letter(df.columns.get_loc(column) + 1)  # Get excel column letter
+            ws.column_dimensions[col_letter].width = max_len
+
+            # Aligning cell values to center
+            for cell in list(ws[col_letter]):
+                cell.alignment = Alignment(horizontal='center')
+
+    book.save(filename)
+
+
+
+
+# # Scrape the pages from the CIK 
+# cik = 1803498
+# filing_soups = scrape(cik)
+
+# # Extract all the relevant tables and dates from each filing
+# tables = []
+# for soup in filing_soups:
+#     result = extract.extract(soup)
+#     if len(result) != 0:
+#         tables.append(result)
+
+tables = extract(soup, "Blackstone Private Credit Fund")
+
+df_tables = []
+for i in range(20):
+    df_tables.append(table_to_dataframe(tables[i][0], tables[i][1]))
+
+write_to_excel(df_tables)
--- code/process.py
+++ code/process.py
@@ -1,5 +1,68 @@
-from bs4 import BeautifulSoup
 import pandas as pd
+import os
+import re
 
-def process(soup_table, date):
-    print("hello world")
+def convert_value(value):
+    # Convert values with only numbers to integer
+    if re.match(r'^[\d,]+$', value):
+        return int(value.replace(',', ''))
+    
+    # Convert percentage formatted values to float (0.xx format)
+    elif '%' in value:
+        try:
+            return float(value.replace('%', '')) / 100
+        except ValueError:
+            pass
+
+    # Convert float in text form to actual float
+    try:
+        return float(value)
+    except ValueError:
+        pass
+
+    # If none of the above, return the value as is
+    return value
+
+def table_to_dataframe(table, date):
+    data = []
+
+    rows = table.find_all('tr')[3:]  # Skip the first row
+
+    headers = []
+    for header in rows[0].find_all('td'):
+        if header.find('span') is None:
+            continue
+        headers.append(header.get_text().strip())
+    data.append(headers)
+
+    for row in rows[1:]:
+        row_data = []
+        for cell in row.find_all('td'):
+            # Skip cells containing only '$' or '%'
+            if cell.get_text().strip() in ["$", "%"]:
+                continue
+
+            # Check if the cell contains a span element, if not skip
+            if cell.find('span') is None:
+                continue
+
+            # Skip cell if it contains a capitalized, three-letter text
+            if len(cell.get_text()) == 3 and cell.get_text().isupper():
+                continue
+
+            cell_value = cell.get_text().strip()
+            converted_value = convert_value(cell_value)
+            row_data.append(converted_value)
+
+        # Combine third and fourth cells and shift left
+        if len(row_data) > 4:
+            row_data[2] = str(row_data[2]) + '         ' + str(row_data[3])
+            del row_data[3]
+        
+        # Append this row data to the overall data
+        if row_data:
+            data.append(row_data)
+    
+    # Convert to DataFrame
+    df = pd.DataFrame(data[1:], columns=data[0])
+    return (df, date)
